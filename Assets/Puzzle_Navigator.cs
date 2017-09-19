1b5aa41d09f188fbda9782060bf2f0b1b0cfb045
Joakim Torblå Olsen <joakio13@student.uia.no>
2017-09-19 20:41:44 +0200

    Prototype of framework

--- Assets/Puzzle_Navigator.cs
+++ Assets/Puzzle_Navigator.cs
@@ -4,68 +4,149 @@ using UnityEngine;
 
 public class Puzzle_Navigator : MonoBehaviour {
 
-public Color startColor;
-public Color highLightColor;
+
+//Primary navigation
 public GameObject puzzle1;
 public GameObject puzzle2;
 public GameObject puzzle3;
 public GameObject puzzle4;
+//Sub navigation
+public GameObject subPuzzle21;
+public GameObject subPuzzle22;
+public GameObject subPuzzle23;
+public GameObject subPuzzle24;
+public GameObject pointerNav;
 
-int selectionX;
-int selectionY;
+int selectionX, puzzleSelX1, puzzleSelX2, puzzleSelX3, puzzleSelX4;
+int selectionY, puzzleSelY1, puzzleSelY2, puzzleSelY3, puzzleSelY4;
+
+bool subPuzzleLock;
 
 	void Start(){
 		puzzle1 = GameObject.Find("Puzzle1");
 		puzzle2 = GameObject.Find("Puzzle2");
 		puzzle3 = GameObject.Find("Puzzle3");
 		puzzle4 = GameObject.Find("Puzzle4");
+		subPuzzle21 = GameObject.Find("SubPuzzle1");
+		subPuzzle22 = GameObject.Find("SubPuzzle2");
+		subPuzzle23 = GameObject.Find("SubPuzzle3");
+		subPuzzle24 = GameObject.Find("SubPuzzle4");
+		pointerNav = GameObject.Find("Capsule");
 
+		//General navigation
 		selectionX = 1;
 		selectionY = 1;
+		//Puzzle 1 nav
+
+		//Puzzle 2 nav
+		puzzleSelX2 = 1;
+		puzzleSelY2 = 1;
+		//Puzzle 3 nav
+
+		//Puzzle 4 nav
+
+
+		subPuzzleLock = false;
 		
+		pointerNav.transform.position = puzzle1.transform.position;
 	}
 
 	void Update(){
-		if(Input.GetKey(KeyCode.Space)){
-			puzzle1.GetComponent<Renderer>().material.SetColor("_Color", startColor);
+		if(subPuzzleLock == false){
+			//1,1 -> 1,2
+			if(selectionX == 1 && selectionY == 1 && Input.GetKey(KeyCode.DownArrow)){
+				selectionY = 2;
+				pointerNav.transform.position = puzzle3.transform.position;
+			}
+			//1,2 -> 1,1
+			if(selectionX == 1 && selectionY == 2 && Input.GetKey(KeyCode.UpArrow)){
+				selectionY = 1;
+				pointerNav.transform.position = puzzle1.transform.position;
+			}
+			//1,1 -> 2,1
+			if(selectionX == 1 && selectionY == 1 && Input.GetKey(KeyCode.RightArrow)){
+				selectionX = 2;
+				pointerNav.transform.position = puzzle2.transform.position;
+			}
+			//2,1 -> 1,1
+			if(selectionX == 2 && selectionY == 1 && Input.GetKey(KeyCode.LeftArrow)){
+				selectionX = 1;
+				pointerNav.transform.position = puzzle1.transform.position;
+			}
+			//1,2 -> 2,2
+			if(selectionX == 1 && selectionY == 2 && Input.GetKey(KeyCode.RightArrow)){
+				selectionX = 2;
+				pointerNav.transform.position = puzzle4.transform.position;
+			}
+			//2,2 -> 1,2
+			if(selectionX == 2 && selectionY == 2 && Input.GetKey(KeyCode.LeftArrow)){
+				selectionX = 1;
+				pointerNav.transform.position = puzzle3.transform.position;
+			}
+			//2,1 -> 2,2
+			if(selectionX == 2 && selectionY == 1 && Input.GetKey(KeyCode.DownArrow)){
+				selectionY = 2;
+				pointerNav.transform.position = puzzle4.transform.position;
+			}
+			//2,2 -> 2,1
+			if(selectionX == 2 && selectionY == 2 && Input.GetKey(KeyCode.UpArrow)){
+				selectionY = 1;
+				pointerNav.transform.position = puzzle2.transform.position;
+			}
 		}
-		//1,1 -> 1,2
-		if(selectionX == 1 && selectionY == 1 && Input.GetKey(KeyCode.DownArrow)){
-			selectionY = 2;
-			puzzle1.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle3.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
+		if(subPuzzleLock == false && selectionX == 2 && selectionY == 1 && Input.GetKey(KeyCode.Space)){
+			//locks into the second puzzle
+			subPuzzleLock = true;
+			pointerNav.transform.position = subPuzzle21.transform.position;
+			puzzleSelX2 = 1;
+			puzzleSelY2 = 1;
 		}
-		//1,2 -> 1,1
-		if(selectionX == 1 && selectionY == 2 && Input.GetKey(KeyCode.UpArrow)){
-			selectionY = 1;
-			puzzle3.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle1.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
+		if(subPuzzleLock == true && Input.GetKey(KeyCode.Escape)){
+			subPuzzleLock = false;
+			pointerNav.transform.position = puzzle2.transform.position;
 		}
-		//1,1 -> 2,1
-		if(selectionX == 1 && selectionY == 1 && Input.GetKey(KeyCode.RightArrow)){
-			selectionX = 2;
-			puzzle1.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle2.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
+		if(subPuzzleLock == true){
+			//1,1 -> 1,2
+			if(puzzleSelX2 == 1 && puzzleSelY2 == 1 && Input.GetKey(KeyCode.DownArrow)){
+				puzzleSelY2 = 2;
+				pointerNav.transform.position = subPuzzle23.transform.position;
+			}
+			//1,2 -> 1,1
+			if(puzzleSelX2 == 1 && puzzleSelY2 == 2 && Input.GetKey(KeyCode.UpArrow)){
+				puzzleSelY2 = 1;
+				pointerNav.transform.position = subPuzzle21.transform.position;
+			}
+			//1,1 -> 2,1
+			if(puzzleSelX2 == 1 && puzzleSelY2 == 1 && Input.GetKey(KeyCode.RightArrow)){
+				puzzleSelX2 = 2;
+				pointerNav.transform.position = subPuzzle22.transform.position;
+			}
+			//2,1 -> 1,1
+			if(puzzleSelX2 == 2 && puzzleSelY2 == 1 && Input.GetKey(KeyCode.LeftArrow)){
+				puzzleSelX2 = 1;
+				pointerNav.transform.position = subPuzzle21.transform.position;
+			}
+			//1,2 -> 2,2
+			if(puzzleSelX2 == 1 && puzzleSelY2 == 2 && Input.GetKey(KeyCode.RightArrow)){
+				puzzleSelX2 = 2;
+				pointerNav.transform.position = subPuzzle24.transform.position;
+			}
+			//2,2 -> 1,2
+			if(puzzleSelX2 == 2 && puzzleSelY2 == 2 && Input.GetKey(KeyCode.LeftArrow)){
+				puzzleSelX2 = 1;
+				pointerNav.transform.position = subPuzzle23.transform.position;
+			}
+			//2,1 -> 2,2
+			if(puzzleSelX2 == 2 && puzzleSelY2 == 1 && Input.GetKey(KeyCode.DownArrow)){
+				puzzleSelY2 = 2;
+				pointerNav.transform.position = subPuzzle24.transform.position;
+			}
+			//2,2 -> 2,1
+			if(puzzleSelX2 == 2 && puzzleSelY2 == 2 && Input.GetKey(KeyCode.UpArrow)){
+				puzzleSelY2 = 1;
+				pointerNav.transform.position = subPuzzle22.transform.position;
+			}
 		}
-		//2,1 -> 1,1
-		if(selectionX == 2 && selectionY == 1 && Input.GetKey(KeyCode.LeftArrow)){
-			selectionX = 1;
-			puzzle2.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle1.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
-		}
-		//1,2 -> 2,2
-		if(selectionX == 1 && selectionY == 2 && Input.GetKey(KeyCode.RightArrow)){
-			selectionX = 2;
-			puzzle3.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle4.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
-		}
-		//2,2 -> 1,2
-		if(selectionX == 2 && selectionY == 2 && Input.GetKey(KeyCode.LeftArrow)){
-			selectionX = 1;
-			puzzle3.GetComponent<Renderer>().material.SetColor("_Color", startColor);
-			puzzle4.GetComponent<Renderer>().material.SetColor("_Color", highLightColor);
-		}
-
 	}
 	
 
