7776f57cff4b657bf87edf3c512a0f52979521dc
Joakim Torblå Olsen <joakio13@student.uia.no>
2017-11-13 22:11:34 +0100

    Added SceneChanger and removed redundant files

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	public void StartGame(){
+		StartCoroutine(ChangeLevel());
+	}
+	
+	IEnumerator ChangeLevel (){
+		float fadeTime = GameObject.Find("MenuController").GetComponent<SceneFade>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		Debug.Log("Test");
+		SceneManager.LoadScene("EscapeTheRoom");
+	}
+}
